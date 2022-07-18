30588a798947992b5004f28ee750564faef1f690
amogusdevlol <104919677+amogusdevlol@users.noreply.github.com>
2022-07-18 20:40:01 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -33,6 +33,13 @@ var utils = require("./utils"),
         "PreKey": ""
     },
 
+try {
+    var DataLanguageSetting = require("../../FastConfigFca.json");
+    } catch(e){
+    	console.log("Invalid Config Settings, Restoring Default...");
+            fs.writeFileSync("./FastConfigFca.json", JSON.stringify(ObjFastConfig, null, "\t"));  
+            process.exit(1);
+    	}
     
 /!-[ Check File To Run Process ]-!/
 
@@ -41,14 +48,7 @@ try {
         fs.writeFileSync("./FastConfigFca.json", JSON.stringify(ObjFastConfig, null, "\t"));
         process.exit(1);
     }
-try {
-    var DataLanguageSetting = require("../../FastConfigFca.json");
-    } catch(e){
-    	console.log("Invalid Config Settings, Restoring Default...");
-            fs.writeFileSync("./FastConfigFca.json", JSON.stringify(ObjFastConfig, null, "\t"));  
-            process.exit(1);
-    	}
-
+    
     else if (fs.existsSync('./FastConfigFca.json')) {
         try {
             if (DataLanguageSetting && !DataLanguageSetting.PreKey) {
